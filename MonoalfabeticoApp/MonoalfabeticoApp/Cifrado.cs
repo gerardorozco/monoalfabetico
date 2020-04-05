02c934b96cd806ca3f4915a609d3610f906b7983
videot uroriales <gerardoorozco1983@gmail.com>
2020-04-05 15:18:22 -0500

    first commit

---
+++ MonoalfabeticoApp/MonoalfabeticoApp/Cifrado.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoalfabeticoApp
+{
+
+    public class Cifrado
+    {
+        public string Clave { get; set; }
+        public string Mensaje { get; set; }
+        public string Resultado { get; set; }
+
+        private const string Alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        public string Cifrar()
+        {
+
+            string newAlfabeto = GenerarAlfabeto();
+            StringBuilder builder = new StringBuilder();
+
+            if (newAlfabeto.Length == Alfabeto.Length)
+            {
+                foreach (char item in this.Mensaje)
+                {
+                    int index = Alfabeto.IndexOf(item);
+                    builder.Append(newAlfabeto[index]);
+                }
+            }
+            else
+            {
+                throw new Exception("Ocurrio un error en la encripcion !");
+            }
+
+            newAlfabeto = builder.ToString();
+
+            return newAlfabeto;
+        }
+
+        public String Descifrar()
+        {
+            string newAlfabeto = GenerarAlfabeto();
+            StringBuilder builder = new StringBuilder();
+
+            if (newAlfabeto.Length == Alfabeto.Length)
+            {
+                foreach (char item in this.Mensaje)
+                {
+                    int index = newAlfabeto.IndexOf(item);
+                    builder.Append(Alfabeto[index]);
+                }
+            }
+            else
+            {
+                throw new Exception("Ocurrio un error en la encripcion !");
+            }
+
+            newAlfabeto = builder.ToString();
+
+            return newAlfabeto;
+        }
+
+        private String GenerarAlfabeto()
+        {
+            this.Clave = EliminarRepetidos(this.Clave).Trim();
+            this.Mensaje = this.Mensaje.ToUpper().Trim();
+
+            StringBuilder builder = new StringBuilder();
+            builder.Append(this.Clave);
+
+            foreach (char item in Alfabeto)
+            {
+                if (!builder.ToString().Contains(item))
+                {
+                    builder.Append(item);
+                }
+            }
+
+            string newAlfabeto = builder.ToString();
+
+            List<char[]> words = new List<char[]>();
+
+            int charIndex = 0;
+
+            builder = new StringBuilder();
+
+            foreach (var item in newAlfabeto)
+            {
+                if (charIndex < this.Clave.Length)
+                {
+                    builder.Append(item);
+                    charIndex++;
+                }
+                else
+                {
+                    words.Add(builder.ToString().ToCharArray());
+                    builder = new StringBuilder();
+                    builder.Append(item);
+                    charIndex = 1;
+                }
+            }
+
+            if (builder.ToString().Length < this.Clave.Length)
+            {
+                Char[] last = new char[Clave.Length];
+
+                charIndex = 0;
+
+                while (charIndex < this.Clave.Length)
+                {
+                    if (charIndex < builder.ToString().Length)
+                    {
+                        last[charIndex] = builder.ToString()[charIndex];
+                    }
+                    else
+                    {
+                        last[charIndex] = '*';
+                    }
+
+                    charIndex++;
+                }
+
+                words.Add(last);
+            }
+
+            builder = new StringBuilder();
+            int fila = 0, columna = 0;
+
+            while (columna < this.Clave.Length)
+            {
+                foreach (char[] item in words)
+                {
+                    builder.Append(item[columna]);
+                    fila++;
+                }
+                columna++;
+            }
+
+            newAlfabeto = builder.ToString();
+
+            if (builder.ToString().Contains('*'))
+            {
+                builder = new StringBuilder();
+
+                foreach (char item in newAlfabeto)
+                {
+                    if (item != '*')
+                    {
+                        builder.Append(item);
+                    }
+                }
+            }
+
+            newAlfabeto = builder.ToString();
+
+            return newAlfabeto;
+        }
+
+        private string EliminarRepetidos(string clave)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            foreach (char item in clave)
+            {
+                if (!builder.ToString().Contains(item))
+                {
+                    builder.Append(item);
+                }
+            }
+
+            return builder.ToString().ToUpper();
+        }
+    }
+}
---
+++ MonoalfabeticoApp/MonoalfabeticoApp/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace MonoalfabeticoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtClave = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMensaje = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCifrar = new System.Windows.Forms.Button();
+            this.btnDescifrar = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtClave
+            // 
+            this.txtClave.Location = new System.Drawing.Point(82, 22);
+            this.txtClave.Name = "txtClave";
+            this.txtClave.Size = new System.Drawing.Size(309, 20);
+            this.txtClave.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Clave";
+            // 
+            // txtMensaje
+            // 
+            this.txtMensaje.Location = new System.Drawing.Point(82, 64);
+            this.txtMensaje.Name = "txtMensaje";
+            this.txtMensaje.Size = new System.Drawing.Size(309, 20);
+            this.txtMensaje.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mensaje";
+            // 
+            // btnCifrar
+            // 
+            this.btnCifrar.Location = new System.Drawing.Point(82, 116);
+            this.btnCifrar.Name = "btnCifrar";
+            this.btnCifrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCifrar.TabIndex = 4;
+            this.btnCifrar.Text = "Cifrar";
+            this.btnCifrar.UseVisualStyleBackColor = true;
+            this.btnCifrar.Click += new System.EventHandler(this.btnCifrar_Click);
+            // 
+            // btnDescifrar
+            // 
+            this.btnDescifrar.Location = new System.Drawing.Point(197, 116);
+            this.btnDescifrar.Name = "btnDescifrar";
+            this.btnDescifrar.Size = new System.Drawing.Size(75, 23);
+            this.btnDescifrar.TabIndex = 5;
+            this.btnDescifrar.Text = "Descifrar";
+            this.btnDescifrar.UseVisualStyleBackColor = true;
+            this.btnDescifrar.Click += new System.EventHandler(this.btnDescifrar_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(82, 171);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 385);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnDescifrar);
+            this.Controls.Add(this.btnCifrar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMensaje);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtClave);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtClave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMensaje;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCifrar;
+        private System.Windows.Forms.Button btnDescifrar;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
